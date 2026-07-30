30cb89e48d3773d94864496a602b372fec7432f3
Aditi Govardhane <aditigovardhane.111@gmail.com>
2026-07-30 16:46:21 +0530

    Added models comment,playlist,like,tweet

---
+++ src/models/playList.model.js
@@ -0,0 +1,28 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const playListSchema = new Schema(
+    {
+        name : {
+            type : String,
+            required: true
+        },
+        discription : {
+            type : String,
+            required : true
+        },
+        videos :[ 
+            {
+                type : Schema.Types.ObjectId,
+                ref : "Video"
+            }
+        ],
+        owner : {
+            type : Schema.Types.ObjectId,
+            ref : "User"            
+        }
+
+    },{timestamps:true}
+)
+
+export const PlayList = mongoose.model("PlayList",playListSchema)
